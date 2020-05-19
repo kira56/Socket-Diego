00f0f938e692cf0729306a540a215aab75aa4ce4
kira56 <ridhunter1508@gmail.com>
2020-05-18 22:52:46 -0500

    socket chat

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,48 +1,47 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios')
+    throw new Error('El nombre y sala son necesarios');
 }
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
 socket.on('connect', function () {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function (resp) {
-        console.log('Usuarios conectados: ', resp)
-    })
+        renderizarUsuarios(resp)
+    });
 
 });
+
 // escuchar
 socket.on('disconnect', function () {
-    console.log('Perdimos conexión con el servidor');
-});
 
-// Enviar información
-// socket.emit('crearMensaje', {
-//     usuario: 'Diego',
-//     mensaje: 'Hola Mundo'
-// }, function (resp) {
-//     console.log('respuesta server: ', resp);
-// });
+    console.log('Perdimos conexión con el servidor');
+
+});
 
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
-
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function (personas) {
-    console.log(personas)
-})
+    renderizarUsuarios(personas)
+});
 
-// mensaje privado
+// Mensajes privados
 socket.on('mensajePrivado', function (mensaje) {
-    console.log('Mensaje Privado', mensaje)
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
